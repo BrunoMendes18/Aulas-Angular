7e25e906d87358e60a5c4122b1644a38ba07a3be
Bruno Mendes <bjsm181818@gmail.com>
2021-03-15 11:29:33 +0000

    das

---
+++ aulas/src/app/experiments/experiments.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experiments',
+  templateUrl: './experiments.component.html',
+  styleUrls: ['./experiments.component.css']
+})
+export class ExperimentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  name:string="Bruno";
+
+}
