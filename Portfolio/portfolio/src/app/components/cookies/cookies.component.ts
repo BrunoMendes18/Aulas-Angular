e5d2faf6b84d01f8c70d25ded0118457f76b708d
BrunoMendes18 <bjsm181818@gmail.com>
2021-06-02 15:24:03 +0100

    cookies code

--- Portfolio/portfolio/src/app/components/cookies/cookies.component.ts
+++ Portfolio/portfolio/src/app/components/cookies/cookies.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CookiesComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+  }
+
+  //esta a dar erro aceiteiCookie:boolean;
+
+  aceitarCookies(ctn:HTMLElement){
+    localStorage.setItem("cookie","1");
+    ctn.style.display="none";
+  }
+
+  lerCookies(){
+    // erro em cima this.aceiteiCookie=localStorage.getItem("cookie")=="1" ? true : false;
   }
 
 }
