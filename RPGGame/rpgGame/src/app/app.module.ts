bed3c9804e0e09ae70723bbdb396c1a288c9ce47
BrunoMendes18 <77855837+BrunoMendes18@users.noreply.github.com>
2021-06-08 19:22:30 +0100

    create components

--- RPGGame/rpgGame/src/app/app.module.ts
+++ RPGGame/rpgGame/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { CreateCharacterComponent } from './components/create-character/create-character.component';
+import { CreateWeaponComponent } from './components/create-weapon/create-weapon.component';
+import { PvpComponent } from './components/pvp/pvp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    CreateCharacterComponent,
+    CreateWeaponComponent,
+    PvpComponent
   ],
   imports: [
     BrowserModule,
